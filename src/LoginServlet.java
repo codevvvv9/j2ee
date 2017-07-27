7f067cf04d91fc8d06f574b5bcd22e6a9d03cbea
Administrator <Administrator@WANGXIANG2.Hisense.ad>
2017-07-27 10:07:24 +0800

    test again

---
+++ src/LoginServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String name = req.getParameter("name");
+		String password = req.getParameter("password");
+		
+		System.out.println("name:" + name);
+		System.out.println("password:" + password);
+	}
+}
